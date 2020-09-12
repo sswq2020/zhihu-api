5a3d87c22343427a1923a6826f16d4cd7ceaea81
shiswq@hletong.com <shiswq@hletong.com>
2020-09-12 20:24:15 +0800

    refactor: 💡 重命名jwt,因为它和koa-jwt太像

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -4,7 +4,7 @@
  */
 
 
-const jwt = require('jsonwebtoken')
+const jsonwebtoken = require('jsonwebtoken') // 这个和koa-jwt完全不同的库
 const {secret} = require('../config')
 
 
@@ -17,7 +17,7 @@ const auth = async (ctx,next) => {
     const {authorization = ''} = ctx.request.header; // header把所有头字段都小写了,前端本来是Authorization
     const token = authorization.replace('Bearer ', ''); // Bearer加空格
     try {
-        const user = jwt.verify(token,secret);
+        const user = jsonwebtoken.verify(token,secret);
         ctx.state.user = user;
     } catch (error) {
         ctx.throw(401,err.message);
