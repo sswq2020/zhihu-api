0e458705b2c4df735cf6388dd9e8c00ea6951e30
shiswq@hletong.com <shiswq@hletong.com>
2020-11-08 14:49:38 +0800

    feat: 🎸 uploads compelete

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
+const koaBody = require('koa-body');
+const koaStatic = require('koa-static')
 const error = require('koa-json-error');
 const parameter = require('koa-parameter');
 const app = new Koa();
+const path = require('path');
 const routing = require('./routes')
 const mongoose = require('mongoose')
 const {connectionStr} = require('./config')
@@ -15,11 +17,18 @@ mongoose.connect(connectionStr,{ useUnifiedTopology: true },()=>{
 mongoose.connection.on('error',console.error)
 
 
+app.use(koaStatic(path.join(__dirname,'public')))
 app.use(error({
     postFormat:(e,{stack,...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack,...rest}
 }))
 
-app.use(bodyParser());
+app.use(koaBody({
+    multipart:true,
+    formidable:{
+        uploadDir: path.join(__dirname,'public/uploads'),
+        keepExtensions:true
+    }
+}));
 app.use(parameter(app));
 routing(app)
 app.listen(2000);
